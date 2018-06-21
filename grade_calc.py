ae1212db3a5066c59274b393ef0dcf0b918daf59
Coursera2017 <32806798+Coursera2017@users.noreply.github.com>
2018-06-20 21:19:38 -0400

    adding grade_calc to GitHub

---
+++ grade_calc.py
@@ -0,0 +1,11 @@
+Grade = 68
+if Grade >= 90 and Grade <= 100:
+  print('A')
+elif Grade >= 80 and Grade <= 89:
+  print('B')
+elif Grade >= 70 and Grade <= 79:
+  print('C')
+elif Grade >= 60 and Grade <= 69:
+  print('D')
+elif Grade < 60:
+  print('F')
