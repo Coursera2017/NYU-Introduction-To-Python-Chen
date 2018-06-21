8fc5aef0e086545eddce34813c5d825ccbc66e01
Coursera2017 <32806798+Coursera2017@users.noreply.github.com>
2018-06-20 21:10:38 -0400

    adding umbrella to GitHub

---
+++ umbrella.py
@@ -0,0 +1,7 @@
+weather = 'sunny'
+if weather == 'rain':
+  print('You will need to bring an umbrella to work with you today.')
+elif weather == 'snow':
+  print('You will need to bring an umbrella to work with you today.')
+else:
+  print('You will not need to bring an umbrella to work with you today.')
