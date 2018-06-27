bded866cfd387ad71201638fb6d48e950a1c9641
Coursera2017 <32806798+Coursera2017@users.noreply.github.com>
2018-06-26 22:03:24 -0400

    adding umbrella to GitHub

---
+++ Homework1.py
@@ -0,0 +1,11 @@
+len(twitter)
+twitter_length = len(twitter)
+if twitter_length < 140
+    print('You still have space left')
+elif twitter_length == 140
+    print('You are just a capacity')
+elif twitter_length > 140
+    print('Your sentence is too long')
+Remaining = 140 - twitter_length
+Remaining = input("How many characters you have remaining to use?")
+print(Remaining)
